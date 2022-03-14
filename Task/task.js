d3a75cd2c1f502222520b4ca98da151e38523cb3
AytacSafarova <100721454+AytacSafarova@users.noreply.github.com>
2022-03-14 04:16:45 -0700

    Add files via upload

---
+++ Task/task.js
@@ -0,0 +1,8 @@
+let number=prompt(" reqem daxil edin:");
+let details=number.split("");
+console.log(details);
+let num1 = details[0];
+let num2= details[1];
+
+console.log( `${num2} is first` );
+console.log( `${num1} is second` );
